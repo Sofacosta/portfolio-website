c764689cfcb428e3104039faf6947545fc0a60df
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-10-07 22:01:43 -0500

    add footer icons and newsletter to footer, added styles

--- src/components/App.js
+++ src/components/App.js
@@ -10,15 +10,13 @@ import '../tailwindImports.css';
 
 const App = () => {
   return (
-    <div className="container mx-auto my-10 max-w-6xl" style={{border: '1px solid red'}}>
+    <div className="container mx-auto my-10 max-w-5xl">
       <Router>
         <header className="flex justify-between pb-12">
           <h1 className="text-5xl font-medium text-white inline-block">Coast2c Lab</h1>
           <Nav />
         </header>
-        <div style={{border: '1px solid blue'}}>
-          <Routes />
-        </div>
+        <Routes />
         <Footer />
       </Router>
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
+import Newsletter from './Newsletter';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faBandcamp, faSoundcloud, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
-    <ul>
-      <li><Link to="https://facebook.com/coast2c"><FontAwesomeIcon icon="fa-brands fa-facebook" /></Link></li>
-      <li><Link to="https://bandcamp.com/coast2c"><FontAwesomeIcon icon="fa-brands fa-bandcamp" /></Link></li>
-      <li><Link to="https://soundcloud.com/coast2c"><FontAwesomeIcon icon="fa-brands fa-soundcloud" /></Link></li>
-      <li><Link to="https://instagram.com/coast2c"><FontAwesomeIcon icon="fa-brands fa-instagram" /></Link></li>
-      <li><Link to="https://youtube.com/coast2c"><FontAwesomeIcon icon="fa-brands fa-youtube" /></Link></li>
-    </ul>
+    <>
+      <ul className='flex justify-center'>
+        <li className='text-white px-3'><Link to="https://facebook.com/coast2c"><FontAwesomeIcon icon={faFacebook} /></Link></li>
+        <li className='text-white px-3'><Link to="https://bandcamp.com/coast2c"><FontAwesomeIcon icon={faBandcamp} /></Link></li>
+        <li className='text-white px-3'><Link to="https://soundcloud.com/coast2c"><FontAwesomeIcon icon={faSoundcloud} /></Link></li>
+        <li className='text-white px-3'><Link to="https://instagram.com/coast2c"><FontAwesomeIcon icon={faInstagram} /></Link></li>
+        <li className='text-white px-3'><Link to="https://youtube.com/coast2c"><FontAwesomeIcon icon={faYoutube} /></Link></li>
+      </ul>
+      <Newsletter />
+    </>
   )
 }
 
