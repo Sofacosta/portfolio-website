8bc7aaa54b98a633966990a0f096708f976c2909
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-10-09 20:11:51 -0500

    Event styling and additional functionality

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import ReactHtmlParser from 'react-html-parser';
 
 const Event = (props) => {
   const { event, isListView } = props;
   const { id, title, subtitle, date, location, price, shortDescription, htmlDescription, infoUrl, buyUrl, thumbnail, originalImg } = event;
   return (
-  <div id={id} className={isListView ? "event event-list-item text-white" : "event event-expanded text-white"}>
-    <img className={isListView ? "event-thumbnail" : "event-hero"} src={isListView ? thumbnail : originalImg} alt="" />
-    <h2>{title}</h2>
-    {(subtitle && !isListView) && <h3>{subtitle}</h3>}
-    <p>Date: {date.toLocaleDateString(navigator.language, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</p>
-    <p>Location: {location}</p>
-    {(price && !isListView) && <p>Price: ${price}</p>}
-    {(shortDescription && !isListView) && <p>{shortDescription}</p>}
-    {(htmlDescription && !isListView) && <span dangerouslySetInnerHTML={{ __html: htmlDescription }} />}
-    {(infoUrl && !isListView) && <a className="info-url" href={infoUrl}>{infoUrl}</a>}
-    {buyUrl && <a className="buy-url" href={buyUrl}>Buy Tickets</a>}
-  </div>
+  <>
+    <hr className="mb-12" />
+    <div id={id} className={isListView ? "event event-list-item text-white" : "event event-expanded text-white"}>
+      <h2 className="text-2xl my-2">{title}</h2>
+      {(subtitle && !isListView) && <h3 className="text-lg mb-1">{subtitle}</h3>}
+      {(thumbnail && originalImg) && <img className={isListView ? "event-thumbnail mb-4" : "event-hero mb-4"} src={isListView ? thumbnail : originalImg} alt="" />}
+      <p><strong>Date:</strong> {date.toLocaleDateString(navigator.language, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</p>
+      <p><strong>Location:</strong> {location}</p>
+      {(price && !isListView) && <p><strong>Price:</strong> ${price}</p>}
+      {(shortDescription && !isListView) && <p className="mt-4">{shortDescription}</p>}
+      {(htmlDescription && !isListView) && <span>{ReactHtmlParser(htmlDescription)}</span>}
+      {(infoUrl && !isListView) && <a className="info-url" href={infoUrl}>{infoUrl}</a>}
+      {buyUrl && <a className="buy-url" href={buyUrl}>Buy Tickets</a>}
+    </div>
+  </>
   );
 };
 
--- src/components/Events.js
+++ src/components/Events.js
@@ -3,10 +3,15 @@ import Event from './Event';
 
 import { events } from '../data/events';
 const Events = () => {
+  const upcomingEvents = events.filter((event) => event.date > new Date());;
+  const pastEvents = events.filter((event) => event.date < new Date());
   return (
     <>
-      <h2 className="text-white text-4xl">Events</h2>
-      {events.map((event) => <Event event={event} isListView={false} key={event.id} />)}
+      <h2 className="text-white text-4xl mb-12">Upcoming Events</h2>
+      {upcomingEvents.length > 0 ? upcomingEvents.map((event) => (<Event event={event} isListView={false} key={event.id} />)) : <p className="text-white my-12 text-center"><em>No upcoming events at the moment, check back soon!</em></p>}
+
+      <h2 className="text-white text-4xl my-12">Past Events</h2>
+      {pastEvents && pastEvents.map((event) => <Event event={event} isListView={false} key={event.id} />)}
     </>
   )
 }
