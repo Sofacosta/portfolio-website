38e478bc19a69e195f66579fdc2c1365c2e528b5
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-11-11 22:06:30 -0600

    edit Vault Ep page

--- src/components/VaultEp.js
+++ src/components/VaultEp.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 const VaultEp = () => {
   return (
-    <h2 className="text-white text-4xl">VaultEp</h2>
+    <>
+      <img class="transform scale-75" src="/assets/images/Vault_cover.png" alt="VaultEP" />
+      <div className="max-w-xl mx-auto pt-4">
+        <p className="text-white justify-center py-4">
+        Vault starts with an hypnotic acid drone piece with “Bones”, a one-take live recording of several different synths, followed by “Time is Not Linear”, an exploration of loops created by the evocative sound of modular synthesizers. “Cornelius” plays with textures, samples and noise. The EP ends with the title track “Vault”, which layers live acid sounds on top of the drums.</p>
+        
+        <p className="text-white justify-center py-4">Vault recieved </p>
+        
+
+      </div>
+    </>
   )
 }
 
