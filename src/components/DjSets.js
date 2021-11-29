7ca6a8b4b87767611dfe76b54a870618cdb2b8ec
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-11-29 17:53:34 -0600

    add Soundcloud mixes link in DJsets page

--- src/components/DjSets.js
+++ src/components/DjSets.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import ReactPlayer from "react-player"
 
 const DjSets = () => {
-  return <h2 className="text-white text-4xl">DJ Sets</h2>;
-};
-
+  return ( 
+    <div className="mx-8">
+      <h2 className="text-white text-4xl pb-8">Radio | Podcast | DJ mixes:</h2>
+      <ReactPlayer
+        url="https://soundcloud.com/coast2c/sets/mixes"
+        height="700px"
+        />
+    </div>
+  )
+}
 export default DjSets;
