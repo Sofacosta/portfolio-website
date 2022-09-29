b2e5f4d402ab8f282dac1420fc89e99a2b3b5788
Sofia Acosta <sofiaacostarascon@gmail.com>
2022-09-29 13:10:30 -0500

    add burger menu for mobile and small screens

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,36 @@
+import { useEffect, useState } from 'react';
 import NavItem from './NavItem';
+import { slide as Menu } from 'react-burger-menu';
 
 import { navItems } from '../data/navItems';
 import '../styles/Nav.css';
 
 const Nav = () => {
-  return (
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 650);
+  const resizeHandler = () => {
+    setDesktop(window.innerWidth > 650);
+  };
+  const getNavItems = navItems.map((item) => {
+    const { path, name, children } = item;
+    return (
+      <NavItem path={path} name={name} children={children} key={name} />
+    );
+  })
+  const desktopNav = (
     <nav className="pt-2.5 inline-block">
-      <ul className="flex justify-start">
-        {navItems &&
-          navItems.map((item) => {
-            const { path, name, children } = item;
-            return (
-              <NavItem path={path} name={name} children={children} key={name} />
-            );
-          })}
-      </ul>
-    </nav>
+        <ul className='flex justify-start'>
+          {navItems && getNavItems}
+        </ul>
+      </nav>
   );
-};
+  const mobileNav = <Menu right>{getNavItems}</Menu>;
+
+  useEffect(() => {
+    window.addEventListener("resize", resizeHandler);
+    return () => window.removeEventListener("resize", resizeHandler);
+  }, []);
+
+  return isDesktop ? desktopNav : mobileNav;
+}
 
 export default Nav;
