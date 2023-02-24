d68ee40b2b10dcf47d1585197ffd0428db2b7853
Sofia Acosta <sofiaacostarascon@gmail.com>
2023-02-23 21:07:43 -0600

    re-add console logs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,11 +18,11 @@ export default function App() {
       .then(
         (result) => {
           alert("Message sent, we'll get back to you shortly");
-          // console.log(result.text);
+          console.log(result.text);
         },
         (error) => {
           alert('An error occurred, Please try again');
-          // console.log(error.text);
+          console.log(error.text);
         }
       )
       .then(() => {
