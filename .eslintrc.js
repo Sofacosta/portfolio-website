005480a9b08b7eca67a2616f85825b92a12faee1
Sofia Acosta <sofiaacostarascon@gmail.com>
2023-10-03 14:38:37 -0600

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,13 @@
 module.exports = {
+  $schema: 'http://json.schemastore.org/eslintrc',
   extends: 'eslint:recommended',
+  files: ['*.js', '*.jsx'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
   rules: {},
-  sourceType: 'module',
 };
